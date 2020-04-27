f236947027c7cdb5f1724dd545501976a2c97ab5
ZhukMax <zhukmax@ya.ru>
2020-04-27 14:24:55 +0300

    Get Args by types

--- src/Request.php
+++ src/Request.php
@@ -2,6 +2,7 @@
 
 namespace Zhukmax\SimpleRouter;
 
+use ReflectionException;
 use ReflectionMethod;
 
 /**
@@ -12,38 +13,49 @@ class Request
 {
     /**
      * @param string $name
-     * @return mixed
+     * @return string
      */
-    public static function get(string $name)
+    public static function get(string $name): string
     {
-        return str_replace(['\'', '"'], ['\\\'', '\\"'], $_REQUEST[$name]) ?? null;
+        return trim(str_replace(['\'', '"'], ['\\\'', '\\"'], $_REQUEST[$name])) ?? null;
     }
-    
+
+    /**
+     * @param string $name
+     * @param bool $default
+     * @return bool
+     */
+    public static function getBool(string $name, bool $default = false): bool
+    {
+        return (bool)$_REQUEST[$name] ?? $default;
+    }
+
     /**
      * @param string $name
      * @param int|null $min
      * @param int|null $max
+     * @param int $default
      * @return int
      */
-    public static function getInt(string $name, int $min = null, int $max = null)
+    public static function getInt(string $name, int $min = null, int $max = null, int $default = 0): int
     {
         $options = [
             'options' => [
-                'default' => 0,
+                'default' => $default,
                 'min_range' => $min ?: -1 * pow(10, 10),
                 'max_range' => $max ?: pow(10, 10)
             ],
             'flags' => FILTER_FLAG_ALLOW_OCTAL
         ];
         
-        return filter_var(self::get($name), FILTER_VALIDATE_INT, $options);
+        return (int)filter_var(self::get($name), FILTER_VALIDATE_INT, $options);
     }
     
     /**
      * @param string $name
      * @return string
      */
-    public static function getEmail(string $name)
+    public static function getEmail(string $name): string
     {
         return filter_var(self::get($name), FILTER_VALIDATE_EMAIL) ?: '';
     }
@@ -52,7 +64,7 @@ class Request
      * @param string $class
      * @param string $method
      * @return array
-     * @throws \ReflectionException
+     * @throws ReflectionException
      */
     public static function getArgs(string $class, string $method): array
     {
@@ -60,9 +72,36 @@ class Request
         $reflection = new ReflectionMethod($class, $method);
 
         foreach($reflection->getParameters() AS $arg) {
-            $args[$arg->name] = self::get($arg->name) ?? null;
+            $args[$arg->name] = self::getArgByType($arg);
         }
 
         return $args;
+    }
+
+    /**
+     * @param \ReflectionParameter $arg
+     * @return bool|int|mixed|string|null
+     */
+    private static function getArgByType(\ReflectionParameter $arg)
+    {
+        try {
+            $default = $arg->getDefaultValue();
+        } catch (ReflectionException $e) {
+            $default = null;
+        }
+
+        switch ($arg->getType()->getName()) {
+            case 'string':
+                return self::get($arg->getName()) ?? $default;
+
+            case 'int':
+                return self::getInt($arg->getName(), null, null, $default);
+
+            case 'bool':
+                return self::getBool($arg->getName(), $default ?? false);
+
+            default:
+                return self::get($arg->getName()) ?? $default;
+        }
     }
 }
